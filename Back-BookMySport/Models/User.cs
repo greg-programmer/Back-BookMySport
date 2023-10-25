e28ed52c13670a0d7b9b245432b172115afd52b2
Grégory <gregory.schoemaecker@outlook.fr>
2023-10-25 15:28:28 +0200

    mise en place des regex User (en cours)

---
+++ Back-BookMySport/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Back_BookMySport.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^\w+([-+.']\w+)*@123g.com$", ErrorMessage = "E-mail invalide.")]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [RegularExpression(@"[^0-9]")]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [RegularExpression(@"[^0-9]")]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
